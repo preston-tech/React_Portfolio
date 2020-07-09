704bc55b80673711a5c640f6d936eb870589b466
preston-tech <prestonphillips42@gmail.com>
2020-07-09 09:32:36 -0600

    working with state

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -7,21 +7,27 @@ export default class PortfolioContainer extends Component {
   constructor() {
     super();
 
-    console.log("Portfolio container has rendered");
+    this.state = {
+      pageTitle: "Welcome to my portfolio",
+      data: [
+        {title: "JCC"}, 
+        {title: "Jiffy Lube"},
+        {title: "Brighton"}
+      ]
+    };
   }
 
   portfolioItems() {
-    const data = ["JCC", "Jiffy Lube", "Brighton"];
 
     return data.map(item => {
-      return <PortfolioItem title={item} url={"google.com"} />;
-    })
+      return <PortfolioItem title={item.title} url={"google.com"} />;
+    });
   }
 
   render() {
     return (
       <div>
-        <h2>Portfolio items go here updated...</h2>
+        <h2>this.state.pageTitle</h2>
 
         {this.portfolioItems()}
       </div>
