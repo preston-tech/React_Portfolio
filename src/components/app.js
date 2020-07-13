96dbddfb6aed93bce4221d35372e7b565c3a9c3a
preston-tech <prestonphillips42@gmail.com>
2020-07-13 16:13:56 -0600

    Working with navlink active styles

--- src/components/app.js
+++ src/components/app.js
@@ -7,6 +7,8 @@ import PortfolioContainer from "./portfolio/portfolio-container";
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
 import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
 
 export default class App extends Component {
   render() {
@@ -17,8 +19,10 @@ export default class App extends Component {
             <NavigationContainer />
 
             <Switch>
-              <Route exact path="/" component={Home} />
+            <Route exact path="/" component={Home} />
               <Route path="/about-me" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/blog" component={Blog} />
             </Switch>
           </div>
         </Router>
