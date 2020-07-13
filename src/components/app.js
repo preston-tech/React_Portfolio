2ef3091d7ff4798c306e5a42198f559a0d704410
preston-tech <prestonphillips42@gmail.com>
2020-07-13 15:50:34 -0600

    React router navLinks

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
 import moment from "moment";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Home from "./pages/home";
+import About from "./pages/about";
 
 import PortfolioContainer from "./portfolio/portfolio-container";
 import NavigationContainer from "./navigation/navigation-container";
@@ -8,8 +15,18 @@ export default class App extends Component {
   render() {
     return (
       <div className="app">
-        <NavigationContainer />
-        <h1>Preston Phillips Portfolio</h1>
+        <Router>
+          <div>
+            <NavigationContainer />
+
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about-me" component={About} />
+            </Switch>
+          </div>
+        </Router>
+
+        <h1>Jordan Hudgens Portfolio</h1>
         <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
         <PortfolioContainer />
       </div>
