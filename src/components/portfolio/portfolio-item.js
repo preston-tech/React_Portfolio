be88264cff553fc18b6b60f25304e1f555232060
preston-tech <prestonphillips42@gmail.com>
2020-07-15 16:17:18 -0600

    Inline styles to render background images

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -2,15 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function(props) {
-  // Data that we'll need:
-  // - background image: thumb_image_url
-  // - logo
-  // - description: description
-  // - id: id
-
   const { id, description, thumb_image_url, logo } = props.item;
   return (
     <div className="portfolio-item-wrapper">
+      <div
+      className="portfolio-img-background"
+      style={{
+        backgroundImage: "url(" + thumb_image_url + ")"
+      }}
+      />
+
+
       <img src={thumb_image_url} />
       <img src={logo} />
       <div>{description}</div>
