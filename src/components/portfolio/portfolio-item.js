7d599052118fd0e6a7ccb86fa3eac63ae4985c48
preston-tech <prestonphillips42@gmail.com>
2020-07-16 14:25:46 -0600

    Updating styles with react event listeners

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-export default function(props) {
-  const { id, description, thumb_image_url, logo_url } = props.item;
+export default class PortfolioItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      portfolioItemClass: ""
+    };
+  }
+
+  handleMouseEnter() {
+    this.setState({portfolioItemClass: 'image-blur'})
+  }
+
+  handleMouseLeave() {
+    this.setState({ portfolioItemClass: ""})
+  }
+
+  render() {
+  const { id, description, thumb_image_url, logo_url } = this.props.item;
   return (
-    <div className="portfolio-item-wrapper">
+    <div className="portfolio-item-wrapper"
+    onMouseEnter={() => this.handleMouseEnter()}
+    onMouseLeave={() => this.handleMouseLeave()}
+    >
       <div
-      className="portfolio-img-background"
+      className={"portfolio-img-background " + this.state.portfolioItemClass}
       style={{
         backgroundImage: "url(" + thumb_image_url + ")"
       }}
@@ -21,4 +41,5 @@ export default function(props) {
       </div>
     </div>
   );
+  }
 }
