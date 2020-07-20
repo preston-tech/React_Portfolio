072cb68e7d9092f9d7f45e7c9df9cada402bc64a
preston-tech <prestonphillips42@gmail.com>
2020-07-20 16:40:01 -0600

    populating portfoliomanager state with API call data

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class PortfolioManager extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            portfolioItems: []
+        }
+    }
+
+    getPortfolioItems() {
+        axios.get("https://prestonphillips.devcamp.space/portfolio/portfolio_items", { withCredentials: true
+        }).then(response => {
+            this.setState({
+                portfolioItems: [...response.data.portfolio_items]
+            });
+        }).catch(error => {
+            console.log("error in getPortfolioItems")
+        })
+    }
+
+    componentDidMount() {
+        this.getPortfolioItems();
+    }
+
     render () {
         return (
             <div className="portfolio-manager-wrapper">
