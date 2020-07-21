267b8b81dfefce6ba2fd52be1cbaf4f429ae6f36
preston-tech <prestonphillips42@gmail.com>
2020-07-21 15:17:41 -0600

    implementing portfolio form handlers

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -16,19 +16,26 @@ export default class PortfolioForm extends Component {
         };
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
-        console.log("handle change", event);
+        this.setState({
+            [event.target.name]: event.target.value
+        });
     }
 
+    handleSubmit(event) {
+        console.log("event", event);
+        event.preventDefault();
+    }
 
     render() {
         return (
             <div>
               <h1>PortfolioForm</h1>
       
-              <form>
+              <form onSubmit={this.handleSubmit}>
                 <div>
                   <input
                     type="text"
