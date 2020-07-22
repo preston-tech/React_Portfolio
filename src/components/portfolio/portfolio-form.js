98eb7c6f03f06cebb69983bee3209c45664d3aca
preston-tech <prestonphillips42@gmail.com>
2020-07-22 15:03:43 -0600

    updating images in API in react

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -1,26 +1,55 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+  
+import React, { Component } from "react";
+import axios from "axios";
+import DropzoneComponent from "react-dropzone-component";
+
+import "../../../node_modules/react-dropzone-component/styles/filepicker.css";
+import "../../../node_modules/dropzone/dist/min/dropzone.min.css";
 
 export default class PortfolioForm extends Component {
-constructor(props) {
+  constructor(props) {
     super(props);
 
     this.state = {
-        name: "",
-        description: "",
-        category: "eCommerce",
-        position: "",
-        url: "",
-        thumb_image: "",
-        banner_image: "",
-        logo: ""
+      name: "",
+      description: "",
+      category: "eCommerce",
+      position: "",
+      url: "",
+      thumb_image: "",
+      banner_image: "",
+      logo: ""
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-}
+    this.componentConfig = this.componentConfig.bind(this);
+    this.djsConfig = this.djsConfig.bind(this);
+    this.handleThumbDrop = this.handleThumbDrop.bind(this);
+  }
 
-buildForm() {
+  handleThumbDrop() {
+    return {
+      addedfile: file => this.setState({ thumb_image: file })
+    };
+  }
+
+  componentConfig() {
+    return {
+      iconFiletypes: [".jpg", ".png"],
+      showFiletypeIcon: true,
+      postUrl: "https://httpbin.org/post"
+    };
+  }
+
+  djsConfig() {
+    return {
+      addRemoveLinks: true,
+      maxFiles: 1
+    };
+  }
+
+  buildForm() {
     let formData = new FormData();
 
     formData.append("portfolio_item[name]", this.state.name);
@@ -29,8 +58,12 @@ buildForm() {
     formData.append("portfolio_item[category]", this.state.category);
     formData.append("portfolio_item[position]", this.state.position);
 
+    if (this.state.thumb_image) {
+      formData.append("portfolio_item[thumb_image]", this.state.thumb_image);
+    }
     return formData;
 }
+
 
 handleChange(event) {
     this.setState({
@@ -50,69 +83,76 @@ handleSubmit(event) {
     console.log("portfolio form handleSubmit error", error);
   });
 
-    event.preventDefault();
+  event.preventDefault();
 }
 
 render() {
-    return (
+  return (
+    <div>
+      <h1>PortfolioForm</h1>
+
+      <form onSubmit={this.handleSubmit}>
         <div>
-          <h1>PortfolioForm</h1>
-  
-          <form onSubmit={this.handleSubmit}>
-            <div>
-              <input
-                type="text"
-                name="name"
-                placeholder="Portfolio Item Name"
-                value={this.state.name}
-                onChange={this.handleChange}
-              />
-  
-              <input
-                type="text"
-                name="url"
-                placeholder="URL"
-                value={this.state.url}
-                onChange={this.handleChange}
-              />
-            </div>
-  
-            <div>
-              <input
-                type="text"
-                name="position"
-                placeholder="Position"
-                value={this.state.position}
-                onChange={this.handleChange}
-              />
-  
-              <select
-                name="category"
-                value={this.state.category}
-                onChange={this.handleChange}
-              >
-                  // Customize values
-                  <option value="eCommerce">eCommerce</option>
-                  <option value="Scheduling">Scheduling</option>
-                  <option value="Enterprise">Enterprise</option>
-              </select>
-            </div>
-  
-            <div>
-              <textarea
-                type="text"
-                name="description"
-                placeholder="Description"
-                value={this.state.description}
-                onChange={this.handleChange}
-              />
-            </div>
-  
-            <div>
-              <button type="submit">Save</button>
-            </div>
-          </form>
+          <input
+            type="text"
+            name="name"
+            placeholder="Portfolio Item Name"
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+
+          <input
+            type="text"
+            name="url"
+            placeholder="URL"
+            value={this.state.url}
+            onChange={this.handleChange}
+          />
         </div>
-      );
-    }
-  }
+
+        <div>
+          <input
+            type="text"
+            name="position"
+            placeholder="Position"
+            value={this.state.position}
+            onChange={this.handleChange}
+          />
+
+          <select
+            name="category"
+            value={this.state.category}
+            onChange={this.handleChange}
+          >
+            <option value="eCommerce">eCommerce</option>
+            <option value="Scheduling">Scheduling</option>
+            <option value="Enterprise">Enterprise</option>
+          </select>
+        </div>
+
+        <div>
+          <textarea
+            type="text"
+            name="description"
+            placeholder="Description"
+            value={this.state.description}
+            onChange={this.handleChange}
+          />
+        </div>
+
+        <div className="image-uploaders">
+          <DropzoneComponent
+            config={this.componentConfig()}
+            djsConfig={this.djsConfig()}
+            eventHandlers={this.handleThumbDrop()}
+          />
+        </div>
+
+        <div>
+          <button type="submit">Save</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+}
