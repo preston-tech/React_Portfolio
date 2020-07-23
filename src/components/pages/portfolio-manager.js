db6ffc4d9dd8738bba679dba8b428c46a11593b5
preston-tech <prestonphillips42@gmail.com>
2020-07-23 16:03:32 -0600

    building an edit click handlerin react

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -9,7 +9,8 @@ export default class PortfolioManager extends Component {
     super();
 
     this.state = {
-      portfolioItems: []
+      portfolioItems: [],
+      portfolioToEdit: {}
     };
 
     this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(
@@ -17,6 +18,12 @@ export default class PortfolioManager extends Component {
     );
     this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
     this.handleDeleteClick = this.handleDeleteClick.bind(this);
+  }
+
+  handleEditClick(portfolioItem) {
+    this.setState({
+      portfolioToEdit: portfolioItem
+    })
   }
 
   handleDeleteClick(portfolioItem) {
@@ -75,6 +82,7 @@ export default class PortfolioManager extends Component {
             <PortfolioSidebarList 
             handleDeleteClick={this.handleDeleteClick}
             data={this.state.portfolioItems} 
+            handleEditClick={this.handleEditClick}
             />
           </div>
         </div>
--- src/components/portfolio/portfolio-sidebar-list.js
+++ src/components/portfolio/portfolio-sidebar-list.js
@@ -1,26 +1,38 @@
 import React from "react";
-import { FortAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const PortfolioSidebarList = props => {
-const portfolioList = props.data.map(portfolioItem => {
-  return (
-    <div key={portfolioItem.id} className="portfolio-item-thumb">
-      <div className="portfolio-thumb-img">
-        <img src={portfolioItem.thumb_image_url} />
-      </div>
+  const portfolioList = props.data.map(portfolioItem => {
+    return (
+      <div key={portfolioItem.id} className="portfolio-item-thumb">
+        <div className="portfolio-thumb-img">
+          <img src={portfolioItem.thumb_image_url} />
+        </div>
 
-      <div className="text-content">
-        <h1 className="title">{portfolioItem.name}</h1>
-        <h2>{portfolioItem.id}</h2>
-        <a className="delete-icon" onClick={() => props.handleDeleteClick(portfolioItem)}>Delete
-          <FontAwesomeIcon icon="trash" />
-        </a>
-      </div>
-    </div>
-  );
-});
+        <div className="text-content">
+          <div className="title">{portfolioItem.name}</div>
 
-return <div className="portfolio-sidebar-list-wrapper">{portfolioList}</div>;
+          <div className="actions">
+            <a
+              className="action-icon"
+              onClick={() => props.handleEditClick(portfolioItem)}
+            >
+              <FontAwesomeIcon icon="edit" />
+            </a>
+
+            <a
+              className="action-icon"
+              onClick={() => props.handleDeleteClick(portfolioItem)}
+            >
+              <FontAwesomeIcon icon="trash" />
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  });
+
+  return <div className="portfolio-sidebar-list-wrapper">{portfolioList}</div>;
 };
 
 export default PortfolioSidebarList;
