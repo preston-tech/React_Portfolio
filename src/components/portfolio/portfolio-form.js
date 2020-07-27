3e78447f885be6fa71a95cfe25aea88962a28aa7
preston-tech <prestonphillips42@gmail.com>
2020-07-27 15:23:50 -0600

    fixing an image edit bug

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -67,9 +67,9 @@ export default class PortfolioForm extends Component {
         editMode: true,
         apiUrl: `https://prestonphillips.devcamp.space/portfolio/portfolio_items/${id}`,
         apiAction: "patch",
-        thumb_image: thumb_image_url || "",
-        banner_image: banner_image_url || "",
-        logo: logo_url || ""
+        thumb_image_url: thumb_image_url || "",
+        banner_image_url: banner_image_url || "",
+        logo_url: logo_url || ""
       });
     }
   }
@@ -229,7 +229,7 @@ render() {
       </div>
 
       <div className="image-uploaders">
-        {this.state.thumb_image && this.state.editMode ? (
+        {this.state.thumb_image_url && this.state.editMode ? (
           <div className="portfolio-manager-image-wrapper">
             <img src={this.state.thumb_image} />
 
@@ -250,7 +250,7 @@ render() {
           </DropzoneComponent>
         )}
 
-        {this.state.banner_image && this.state.editMode ? (
+        {this.state.banner_image_url && this.state.editMode ? (
           <div className="portfolio-manager-image-wrapper">
             <img src={this.state.banner_image} />
 
@@ -271,7 +271,7 @@ render() {
           </DropzoneComponent>
         )}
 
-        {this.state.logo && this.state.editMode ? (
+        {this.state.logo_url && this.state.editMode ? (
           <div className="portfolio-manager-image-wrapper">
             <img src={this.state.logo} />
 
