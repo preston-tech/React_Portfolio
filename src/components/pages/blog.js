a9e89c84cabf05e4a701dce89e54ed20cef1c90c
preston-tech <prestonphillips42@gmail.com>
2020-07-28 14:55:48 -0600

    calling a single blog item from the api

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
+import BlogItem from "../blog/blog-item";
 
 class Blog extends Component {
   constructor() {
@@ -8,23 +9,24 @@ class Blog extends Component {
 
     this.state = {
       blogItems: []
-    }
+    };
 
     this.getBlogItems = this.getBlogItems.bind(this);
   }
 
   getBlogItems() {
     axios
-    .get(
-      "https://prestonphillips.devcamp.space/portfolio/portfolio_blogs", {
-         withCredentials: true 
-        }).then(response => {
-          this.setState({
-            blogItems: response.data.portfolio_blogs
-          })
-        }).catch(error => {
-          console.log("getBlogItems error", error);
-        })
+      .get("https://prestonphillips.devcamp.space/portfolio/portfolio_blogs", {
+        withCredentials: true
+      })
+      .then(response => {
+        this.setState({
+          blogItems: response.data.portfolio_blogs
+        });
+      })
+      .catch(error => {
+        console.log("getBlogItems error", error);
+      });
   }
 
   componentWillMount() {
@@ -33,14 +35,10 @@ class Blog extends Component {
 
   render() {
     const blogRecords = this.state.blogItems.map(blogItem => {
-    return <h1>{blogItem.title}</h1>;
+      return <BlogItem key={blogItem.id} blogItem={blogItem} />;
     });
 
-    return (
-      <div>
-        {blogRecords}
-      </div>
-    );
+    return <div>{blogRecords}</div>;
   }
 }
 
