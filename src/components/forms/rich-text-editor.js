553e5e18b25ac72a42f3c0efb891c28b69061aa4
preston-tech <prestonphillips42@gmail.com>
2020-07-30 16:16:52 -0600

    retrieving html content from draft js component

--- src/components/forms/rich-text-editor.js
+++ src/components/forms/rich-text-editor.js
@@ -5,23 +5,35 @@ import draftToHtml from 'draftjs-to-html';
 import htmlToDraft from 'html-to-draftjs';
 
 export default class RichTextEditor extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            editorState: EditorState.createEmpty()
-        }
-    }
+    this.state = {
+        editorState: EditorState.createEmpty()
+    };
 
-    render() {
-        return (
-            <div>
-                <Editor
-                editorState={this.state.editorState}
-                wrapperClassName="demo-wrapper"
-                editorClassName="demo-editor"
-                />
-            </div>
-        )
-    }
+    this.onEditorStateChange = this.onEditorStateChange.bind(this);
+  }
+
+  onEditorStateChange(editorState) {
+    this.setState(
+      { editorState },
+      this.props.handleRichTextEditorChange(
+        draftToHtml(convertToRaw(this.state.editorState.getCurrentContent()))
+      )
+    );
+  }
+
+  render() {
+    return (
+        <div>
+          <Editor
+          editorState={this.state.editorState}
+          wrapperClassName="demo-wrapper"
+          editorClassName="demo-editor"
+          onEditorStateChange={this.onEditorStateChange}
+          />
+        </div>
+    )
+  }
 }
