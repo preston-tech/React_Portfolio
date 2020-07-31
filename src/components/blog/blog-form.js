1723d6da31ef94094c32d986c71bb107283ac3fc
preston-tech <prestonphillips42@gmail.com>
2020-07-31 14:24:27 -0600

    adding the ability to upload featured images to blog

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -20,6 +20,9 @@ export default class BlogForm extends Component {
     this.componentConfig = this.componentConfig.bind(this);
     this.djsConfig = this.djsConfig.bind(this);
     this.handleFeaturedImageDrop = this.handleFeaturedImageDrop.bind(this);
+
+    this.featuredImageRef = React.createRef();
+
   }
 
   handleFeaturedImageDrop() {
@@ -53,6 +56,10 @@ export default class BlogForm extends Component {
     formData.append("portfolio_blog[blog_status]", this.state.blog_status)
     formData.append("portfolio_blog[content]", this.state.content)
 
+    if (this.state.featured_image) {
+      formData.append("portfolio_blog[featured_image]", this.state.featured_image);
+    }
+
     return formData;
   }
 
@@ -64,11 +71,17 @@ export default class BlogForm extends Component {
         { withCredentials: true }
       )
       .then(response => {
+        if (this.state.featured_image) {
+          this.featuredImageRef.current.dropzone.removeAllFiles();
+        }
+
         this.setState({
           title: "",
           blog_status: "",
-          content: ""
+          content: "",
+          featured_image: ""
         });
+
   
         this.props.handleSuccessfulFormSubmission(
           response.data.portfolio_blog
@@ -116,6 +129,7 @@ export default class BlogForm extends Component {
 
         <div className="image-uploaders">
           <DropzoneComponent
+            ref={this.featuredImageRef}
             config={this.componentConfig()}
             djsConfig={this.djsConfig()}
             eventHandlers={this.handleFeaturedImageDrop()}
