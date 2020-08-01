c692ddac01d813c4e87cd687b8fc17ef6f3c73d3
preston-tech <prestonphillips42@gmail.com>
2020-08-01 13:33:18 -0600

    building about page styels

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,5 +1,19 @@
 import React from "react";
+import profilePicture from "../../../static/assets/images/headshot.jpg";
 
 export default function() {
-  return <div>About</div>;
+  return (
+  <div className="content-page-wrapper">
+    <div className="left-column"
+    style={{
+      background: "url(" + profilePicture + ") no-repeat",
+      backgroundSize: "cover",
+      backgroundPosition: "center"
+    }}
+    />
+    <div className="right-column">
+
+    </div>
+  </div>
+  )
 }
