8016b4cd4040d84dc1810fe169bbd9b5e4f7d1e9
preston-tech <prestonphillips42@gmail.com>
2020-08-02 21:48:43 -0600

    initial build out of the portfolio Detail component

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -1,10 +1,32 @@
-  
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-export default function(props) {
-  return (
-    <div>
-      <h2>Portfolio Detail for {props.match.params.slug}</h2>
-    </div>
-  );
+export default class PortfolioDetail extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillMount() {
+    this.getPortfolioItem();
+  }
+
+  getPortfolioItem() {
+    axios
+    .get(
+      `https://prestonphillips.devcamp.space/portfolio/portfolio_items/${this.props.match.params.slug}`,
+      { withCredentials: true }
+    )
+      .then(response => {
+        console.log("response", response)
+      }).catch(error => {
+        console.log("getPortfolioitem error", error);
+    })
+  }
+  render() {
+    return (
+      <div>
+        <h2>Portfolio Detail for {this.props.match.params.slug}</h2>
+      </div>
+    )
+  }
 }
