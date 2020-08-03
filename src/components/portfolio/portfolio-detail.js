6ebd920ad1fb519a98cc666155b0061409c674fe
preston-tech <prestonphillips42@gmail.com>
2020-08-02 21:56:02 -0600

    Rendering the portfolio detail data on the screen

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -4,6 +4,10 @@ import axios from "axios";
 export default class PortfolioDetail extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      portfolioItem: {}
+    };
   }
 
   componentWillMount() {
@@ -17,15 +21,28 @@ export default class PortfolioDetail extends Component {
       { withCredentials: true }
     )
       .then(response => {
-        console.log("response", response)
+        this.setState({
+          portfolioItem: response.data.portfolio_item
+        })
       }).catch(error => {
         console.log("getPortfolioitem error", error);
     })
   }
   render() {
+    const{
+    banner_image_url,
+    category,
+    description,
+    logo_url,
+    name, 
+    thumb_image_url,
+    url
+    } = this.state.portfolioItem;
+
     return (
       <div>
-        <h2>Portfolio Detail for {this.props.match.params.slug}</h2>
+        <h2>{name}</h2>
+        <h2>{description}</h2>
       </div>
     )
   }
